64fc6b76c02f2d686f14c5eeb57ea8cd9506749f
almeida-matheus <matheusalmeidacosta1212@gmail.com>
2022-05-29 00:11:22 -0300

    add dynamic site - js and base content

---
+++ js/main.js
@@ -0,0 +1,36 @@
+import { user, resources } from './modules/data.js'
+
+async function WriteTextContent(resources) {
+
+    document.title = user.UserName
+    document.getElementById('header__name').innerHTML = user.UserName
+    document.getElementById('header__img').src = user.ImageProfile
+
+    resources.forEach((service) => {
+        if (service.Visibility === 'Hidden')
+            return
+
+        const a = document.createElement('a')
+        const div = document.createElement('div')
+        const i =document.createElement('i')
+        i.setAttribute('class', service.IconFontawesome)
+        i.style.color = service.PrimaryColor
+        a.setAttribute('href', service.PersonalURL)
+        a.setAttribute('target', '_blank')
+        a.setAttribute('rel', 'noopener')
+        a.innerHTML = service.ServiceName
+        document.getElementById('list__body').append(a)
+        div.append(i)
+        a.prepend(div)
+    });
+
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        response = await WriteTextContent(resources)
+    }
+    catch (e) {
+        console.log('Ocorreu um erro',e)
+    }
+})
---
+++ js/modules/data.js
@@ -0,0 +1,206 @@
+const user = {
+    UserId: "bad79324-c345-11ec-9d64-0242ac120002",
+    UserName: "Matheus Almeida",
+    Description: "",
+    ImageProfile: "./assets/image/matheus-almeida.jpeg"
+}
+
+const resources = [
+    {
+        ServiceName: "Portfólio",
+        PersonalURL: "https://almeidamatheus.me/",
+        PrimaryColor: "#323232",
+        IconFontawesome: "fa-solid fa-user",
+        // IconFontawesome: "fa-solid fa-user-tie",
+        TypeService: "Social",
+        Nickname: "",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Linkedin",
+        PersonalURL: "https://www.linkedin.com/in/matheus-almeida-costa/",
+        PrimaryColor: "#3b5999",
+        IconFontawesome: "fa-brands fa-linkedin-in",
+        TypeService: "Social",
+        Nickname: "Matheus Almeida Costa",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Blog",
+        PersonalURL: "https://blog.almeidamatheus.me/",
+        PrimaryColor: "#01633e",
+        // IconFontawesome: "fa-solid fa-book",
+        // IconFontawesome: "fa-solid fa-newspaper",
+        IconFontawesome: "fa-solid fa-square-pen",
+        TypeService: "Social",
+        Nickname: "",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Medium",
+        PersonalURL: "https://almeida-matheus.medium.com/",
+        PrimaryColor: "#009F00",
+        IconFontawesome: "fa-brands fa-medium-m",
+        TypeService: "Social",
+        Nickname: "almeida-matheus",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Youtube",
+        PersonalURL: "https://www.youtube.com/c/matheusalmeidac",
+        PrimaryColor: "#c4302b",
+        IconFontawesome: "fa-brands fa-youtube",
+        TypeService: "Social",
+        Nickname: "Matheus Almeida",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Instagram",
+        PersonalURL: "https://www.instagram.com/almeidamatheus.me/",
+        PrimaryColor: "#E1306C",
+        IconFontawesome: "fa-brands fa-instagram",
+        TypeService: "Social",
+        Nickname: "almeidamatheus.me",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "GitHub",
+        PersonalURL: "https://github.com/almeida-matheus",
+        PrimaryColor: "#333333",
+        IconFontawesome: "fa-brands fa-github",
+        TypeService: "Social",
+        Nickname: "almeida-matheus",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Spotify",
+        PersonalURL: "https://open.spotify.com/user/toycbdrujjrwpyk66pusui1hl",
+        PrimaryColor: "#1DB954",
+        IconFontawesome: "fa-brands fa-spotify",
+        TypeService: "Social",
+        Nickname: "Matheus Almeida",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Twitter",
+        PersonalURL: "https://twitter.com/almeidamatheusm",
+        PrimaryColor: "#1DA1F2",
+        IconFontawesome: "fa-brands fa-twitter",
+        TypeService: "Social",
+        Nickname: "almeidamatheusm",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Discord",
+        PersonalURL: "https://discordapp.com/users/shiryu#2186",
+        PrimaryColor: "#7289da",
+        IconFontawesome: "fa-brands fa-discord",
+        TypeService: "Social",
+        Nickname: "shiryu#2186",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Reddit",
+        PersonalURL: "https://www.reddit.com/user/matheuscbjr",
+        PrimaryColor: "#ff6314",
+        IconFontawesome: "fa-brands fa-reddit",
+        TypeService: "Social",
+        Nickname: "matheuscbjr",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Steam",
+        PersonalURL: "https://steamcommunity.com/id/shiryunk/",
+        PrimaryColor: "#171a21",
+        IconFontawesome: "fa-brands fa-steam",
+        TypeService: "Social",
+        Nickname: "shiryunk",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Telegram",
+        PersonalURL: "https://t.me/almeidamatheus",
+        PrimaryColor: "#0088cc",
+        IconFontawesome: "fa-brands fa-telegram-plane",
+        TypeService: "Social",
+        Nickname: "almeidamatheus",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Pinterest",
+        PersonalURL: "https://br.pinterest.com/almeidamatheus_/",
+        PrimaryColor: "#E60023",
+        IconFontawesome: "fa-brands fa-pinterest",
+        TypeService: "Social",
+        NickName: "",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "E-mail",
+        PersonalURL: "mailto:almeidamatheus.m@gmail.com",
+        PrimaryColor: "#004f9f",
+        IconFontawesome: "fa-solid fa-envelope",
+        TypeService: "Social",
+        NickName: "almeidamatheus.m@gmail.com",
+        Visibility: "Visible"
+    },
+    {
+        ServiceName: "Ubi Connect/Uplay",
+        PersonalURL: "",
+        PrimaryColor: "#323232",
+        IconFontawesome: "fa-solid fa-gamepad",
+        TypeService: "Social",
+        NickName: "shiryunk",
+        Visibility: "Hidden"
+    },
+    {
+        ServiceName: "EA Play/Origin",
+        PersonalURL: "",
+        PrimaryColor: "#323232",
+        IconFontawesome: "fa-solid fa-gamepad",
+        TypeService: "Social",
+        NickName: "shiryunk",
+        Visibility: "Hidden"
+    },
+    {
+        ServiceName: "Epic Games",
+        PersonalURL: "",
+        PrimaryColor: "#323232",
+        IconFontawesome: "fa-solid fa-gamepad",
+        TypeService: "Social",
+        NickName: "CharlieBrownJr",
+        Visibility: "Hidden"
+    },
+    {
+        ServiceName: "Battle.net",
+        PersonalURL: "",
+        PrimaryColor: "#323232",
+        IconFontawesome: "fa-solid fa-gamepad",
+        TypeService: "Social",
+        NickName: "MatheusCBJR#1347",
+        Visibility: "Hidden"
+    },
+    {
+        ServiceName: "GOG",
+        PersonalURL: "https://www.gog.com/u/CharlieBrownJr",
+        PrimaryColor: "#323232",
+        IconFontawesome: "fa-solid fa-gamepad",
+        TypeService: "Social",
+        NickName: "CharlieBrownJr",
+        Visibility: "Hidden"
+    },
+    {
+        ServiceName: "Riot",
+        PersonalURL: "",
+        PrimaryColor: "#323232",
+        IconFontawesome: "fa-solid fa-gamepad",
+        TypeService: "Social",
+        NickName: "shiryunk#1234",
+        Visibility: "Hidden"
+    }
+]
+
+export {
+    user,
+    resources
+}
